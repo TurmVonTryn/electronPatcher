c5b185f2bf6cb604c20a7e17b7c9c6002d195b2e
schorty <schorty@users.noreply.github.com>
2022-01-05 12:46:09 +0100

    Initial commit

---
+++ preload.js
@@ -0,0 +1,19 @@
+// All of the Node.js APIs are available in the preload process.
+// It has the same sandbox as a Chrome extension.
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector)
+    if (element) element.innerText = text
+  }
+
+  for (const type of ['chrome', 'node', 'electron']) {
+    replaceText(`${type}-version`, process.versions[type])
+  }
+})
+
+const { contextBridge, ipcRenderer } = require('electron')
+
+contextBridge.exposeInMainWorld('files', {
+  read: () => ipcRenderer.invoke('files:read'),
+  get: () => ipcRenderer.invoke('files:get')
+})
